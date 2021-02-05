41138208f40840e4274bca9d42b1cc87ac84b8dc
stepin104974 <69135861+stepin-104974-sumit@users.noreply.github.com>
2021-02-05 22:51:03 +0530

    xcscc

--- Implementation/src/cuberoot.c
+++ Implementation/src/cuberoot.c
@@ -1,9 +1,5 @@
 #include "cuberoot.h"
-<<<<<<< Updated upstream
 //function to find the cuberoot
-=======
-
->>>>>>> Stashed changes
 double cuberoot(double x){
     double i,res, precision = 0.000001;
     for(i=1;(i*i*i)<=x;i++);
