9204a7087b92a1fa2e19f86c5485455de796d91a
99003538 <69082645+PS-99003538@users.noreply.github.com>
2021-02-05 19:06:51 +0530

    1

---
+++ Implementation/test/test-exponential.c
@@ -0,0 +1,14 @@
+#include "exponential.h"
+
+void setUp()
+{    }
+
+void tearDown()
+{    }
+
+void test_exponential(void)
+{
+    TEST_ASSERT_EQUAL(2.17, exponential(1);
+    TEST_ASSERT_EQUAL(0.36, exponential(-1,0.36));
+   
+}
