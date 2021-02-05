33527430ff40aef2ab691370c4ed3700ae3a9fb5
99003536 <78536005+99003536@users.noreply.github.com>
2021-02-05 20:34:58 +0530

    Add files via upload

---
+++ Implementation/test/test_cuberoot.c
@@ -0,0 +1,14 @@
+#include "cuberoot.h"
+
+void setUp()
+{    }
+
+void tearDown()
+{    }
+
+void test_add(void)
+{
+    TEST_ASSERT_EQUAL(0.000000, cuberoot(0);
+    TEST_ASSERT_EQUAL(5.000000, cuberoot(125));
+    TEST_ASSERT_EQUAL(4.641589, cuberoot(100);
+}
