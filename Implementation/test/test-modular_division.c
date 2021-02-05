99596bfc87dbecca9c7455b0ea04df72af832e80
namratha <namrathashankarm@gmail.com>
2021-02-05 18:13:49 +0530

    Update test-modular_division.c

--- Implementation/test/test-modular_division.c
+++ Implementation/test/test-modular_division.c
@@ -8,7 +8,7 @@ void tearDown()
 
 void test_modular_division(void)
 {
-    TEST_ASSERT_EQUAL(4, modular_division(4,8));
-    TEST_ASSERT_EQUAL(0, modular_division(10,5));
-    TEST_ASSERT_EQUAL(1, modular_division(10, 3);
+    TEST_ASSERT_EQUAL(4, modular_division_int(4,8));
+    TEST_ASSERT_EQUAL(0, modular_division_int(10,5));
+    TEST_ASSERT_EQUAL(1.9, modular_division_float(10.3, 4.2);
 }
