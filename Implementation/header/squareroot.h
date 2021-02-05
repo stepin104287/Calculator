8e0f9d43f425f0d299a182a6cd01b54a3b9a729a
Psnumber-3535 <rishita.thotakura@ltts.com>
2021-02-05 19:14:13 +0530

    Update Sqrt

---
+++ Implementation/header/squareroot.h
@@ -0,0 +1,6 @@
+#ifndef __CUBEROOT_H__
+#define __CUBEROOT_H__
+
+int squareroot(int x);
+
+#endif
---
+++ Implementation/src/squareroot.c
@@ -0,0 +1,13 @@
+#include "squareroot.h"
+#include <stdio.h>
+#include <math.h>
+
+int squareroot(int x)
+{
+    float result;
+
+    result=sqrt(x);
+    
+    return result;
+
+}
