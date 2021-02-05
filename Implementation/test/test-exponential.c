2fc07dc9fc7d7d8ccdfa0bc156c661e98a1cca4c
99003538 <78536501+99003538@users.noreply.github.com>
2021-02-05 21:50:43 +0530

    Update test-exponential.c

--- Implementation/test/test-exponential.c
+++ Implementation/test/test-exponential.c
@@ -8,7 +8,8 @@ void tearDown()
 
 void test_exponential(void)
 {
-    TEST_ASSERT_EQUAL(2.17, exponential(1);
-    TEST_ASSERT_EQUAL(0.36, exponential(-1,0.36));
+    TEST_ASSERT_EQUAL(2.178000, exponential(1);
+    TEST_ASSERT_EQUAL(1.000000, exponential(0));
+    TEST_ASSERT_EQUAL(0.367918, exponential(-1);
    
 }
