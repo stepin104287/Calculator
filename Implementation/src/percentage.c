185c142400853c2392417a42448227adf5e4f294
stepin104974 <69135861+stepin-104974-sumit@users.noreply.github.com>
2021-02-05 23:31:29 +0530

    a

--- Implementation/src/percentage.c
+++ Implementation/src/percentage.c
@@ -1,12 +1,12 @@
 #include "percentage.h"
+#include<stdio.h>
 
 float percentage(float num1, float num2)
-
 {
-   float num1,num2;
+   //float num1,num2;
    float total;
-   printf("Enter two numbers\n");
-   scanf("%f %f",&num1,&num2);
+   //printf("Enter two numbers\n");
+   //scanf("%f %f",&num1,&num2);
    //total=((num2-num1)*100)/num1;
    total=((num1*num2)/100);
    //printf("%f",total);
