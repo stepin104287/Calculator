d2bbf5402149b900c97809059010c1231e67c6b1
namratha <namrathashankarm@gmail.com>
2021-02-05 18:10:45 +0530

    Create test-modular_division.c

---
+++ Implementation/test/test-modular_division.c
@@ -0,0 +1,14 @@
+#include "modular_division.h"
+
+void setUp()
+{    }
+
+void tearDown()
+{    }
+
+void test_modular_division(void)
+{
+    TEST_ASSERT_EQUAL(4, modular_division(4,8));
+    TEST_ASSERT_EQUAL(0, modular_division(10,5));
+    TEST_ASSERT_EQUAL(1, modular_division(10, 3);
+}
