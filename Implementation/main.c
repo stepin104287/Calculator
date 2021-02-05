00dd3818ddac85c84d02b70bb14f504beaf41bfd
stepin104974 <69135861+stepin-104974-sumit@users.noreply.github.com>
2021-02-05 21:59:48 +0530

    as

--- Implementation/main.c
+++ Implementation/main.c
@@ -1,30 +1,39 @@
 #include<stdio.h>
-#include "header/subtraction.h"
-#include "header/add.h"
+#include "subtraction.h"
+#include "add.h"
+#include "cuberoot.h"
+#include "divide.h"
+#include "exponential.h"
+#include "modular_division"
+#include "multiplication.h"
+#include "percentage.h"
+#include "squareroot.h"
+
 
 int main()
 {
 
     //first input from user: a number
-    printf("Hello user, please enter the first number \n");
-    int A=0;
+    int A,B;
+    printf("Hello user, please enter the 1st numbers \n");
     scanf("%d", &A);
+    printf("Hello user, please enter the 2nd numbers \n");
+    scanf("%d", &B);
     
 
     //users choice 
     char choice='y';
     
-    while (choice=='y')
-    {   int operation;
-        printf("hello user, you can do operation now please proceed\n");
-        printf("enter your choice for function as mentioned further:\n ");
-        printf("+: 0\n-: 1\nx: 2\n/: 3\nmod. division: 4\nsqrt: 5\npercentage: 6\ncube_root: 7\nexpo.: 8\nexit: 9");
-        scanf("%d",&operation);
+    int operation;
+    printf("hello user, you can do operation now please proceed\n");
+    printf("enter your choice for function as mentioned further:\n ");
+    printf("+: 0\n-: 1\nx: 2\n/: 3\nmod. division: 4\nsqrt: 5\npercentage: 6\ncube_root: 7\nexpo.: 8\nexit: 9");
+    scanf("%d",&operation);
         
         
-        //switching functions
-        switch (operation)
-        {
+    //switching functions
+    switch (operation)
+    {
 
         //exit choice{
         case 9:
@@ -35,20 +44,25 @@ int main()
         //addition function
         case 0:
             //call your Addition fuunction
-            break;    return 0;
-            //add your multiplication function
-            break;
+            break;    
+            
 
-        //division function
+        //simple division function
         case 3:
-            //add your division function 
+            //add your simple division function 
             break;
 
-        //modulus division function 
+        //modulus modulardivision function 
         case 4:
-                return 0;
+                
+            //add your modulardivision function
+            break;
+        
+        //sqrt function
+        case 5:
             //add your sqrt function
             break;
+
 
         //percentage function
         case 6:
@@ -71,8 +85,7 @@ int main()
         
         default:
             break;
-        }
-
     }
-    
+
+  
 return 0;}
