1b6a2ed670a6ef10546e20827af77564a7646d15
Harshitha r <harshirharshitha@gmail.com>
2021-02-06 16:14:12 +0530

    Update multiplication.c

--- Implementation/src/multiplication.c
+++ Implementation/src/multiplication.c
@@ -4,7 +4,18 @@
 int multiplication(int x,int y)
 {
     int product;
+    if(x==0)
+    {
+        return 0;
+    }
+    else if(y==0)
+    {
+        return 0;
+    }
+    else
+    {
     product=x*y;
     return product;
+    }
 }
 
