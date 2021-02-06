cd9cc60f9018d4a4a7e7973fbed58e392de2e860
stepin104974 <69135861+stepin-104974-sumit@users.noreply.github.com>
2021-02-06 08:17:49 +0530

    finlised test

---
+++ Implementation/test/test_functions.c
@@ -0,0 +1,101 @@
+
+#include<stdio.h>
+#include "subtraction.h"
+#include "add.h"
+#include "cuberoot.h"
+#include "divide.h"
+#include "exponential.h"
+#include "modular_division.h"
+#include "multiplication.h"
+#include "percentage.h"
+#include "squareroot.h"
+
+
+
+void setUp(){}
+
+void tearDown(){}
+
+void test_Duycycle(void){                  //test function1
+
+    printf("\ntest_Dutycycle executed\n");
+
+    TEST_ASSERT_EQUAL(50, 100*Dutycycle(50));
+
+    TEST_ASSERT_EQUAL(100, 100*Dutycycle(100));
+
+}
+
+
+
+void test_add(void)
+{
+    //TEST_ASSERT_EQUAL(0.000000, cuberoot(0);
+    TEST_ASSERT_EQUAL(5.000000, cuberoot(125));
+    //TEST_ASSERT_EQUAL(4.641589, cuberoot(100);
+}
+void test_percentage(void)
+{
+    TEST_ASSERT_EQUAL(15, percentage(15,100));
+    //TEST_ASSERT_EQUAL(20, percentage(20, 100));
+    //TEST_ASSERT_EQUAL(3.90, percentage(15, 26));
+    
+    //TEST_ASSERT_EQUAL(30, percentage(15,100));
+    //TEST_ASSERT_EQUAL(3.0, percentage(15,26));
+}
+
+void test_add(void)
+{
+    TEST_ASSERT_EQUAL(12, add(4,8));
+    TEST_ASSERT_EQUAL(-30, add(-50, 20));
+    TEST_ASSERT_EQUAL(12.4, add(8.2, 4.2));
+}
+void test_divide(void)
+{
+    TEST_ASSERT_EQUAL(3, divide_int(9,3));
+    TEST_ASSERT_EQUAL(0, divide_int(0,1));
+    TEST_ASSERT_EQUAL(2.45, divide_float(10.3,4.2));
+}
+void test_exponential(void)
+{
+    TEST_ASSERT_EQUAL(2.178000, exponential(1);
+    TEST_ASSERT_EQUAL(1.000000, exponential(0));
+    TEST_ASSERT_EQUAL(0.367918, exponential(-1);
+   
+}
+void test_modular_division(void)
+{
+    TEST_ASSERT_EQUAL(4, modular_division(4,8));
+    TEST_ASSERT_EQUAL(0, modular_division(10,5));
+    TEST_ASSERT_EQUAL(1.9, modular_division(10.3, 4.2);
+}
+void test_multiplication(void)
+{
+    TEST_ASSERT_EQUAL(20,multiplication(4,5))
+    TEST_ASSERT_EQUAL(43.800003,multiplication(6,7.3))
+    TEST_ASSERT_EQUAL(15.400000,multiplication(2.2,7))
+}
+
+void test_add(void)
+{
+    TEST_ASSERT_EQUAL(2, squareroot(4);
+    TEST_ASSERT_EQUAL(0, squareroot(0));
+    TEST_ASSERT_EQUAL(1.581, squareroot(2.5);
+}
+void test_subtraction(void){
+    TEST_ASSERT_EQUAL(1,subtraction(2,1));
+}
+
+
+int test_main(void){
+
+
+    UNITY_BEGIN();
+
+
+    RUN_TEST(test_Duycycle);
+
+    RUN_TEST(test_set_iload);
+
+    return UNITY_END();
+}
