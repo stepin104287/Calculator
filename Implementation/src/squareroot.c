0c55dfa543e567fdc37805660856c0205352c521
Psnumber-3535 <rishita.thotakura@ltts.com>
2021-02-06 15:53:16 +0530

    sqrt

--- Implementation/src/squareroot.c
+++ Implementation/src/squareroot.c
@@ -2,14 +2,26 @@
 #include <stdio.h>
 #include <math.h>
 
-int squareroot(int x)
+int squareroot(int number)
 {
-    float result;
 
-    result=sqrt(x);
-
-    printf("%f\n",result);
-    
-    return result;
+    if (number<1)
+    {
+        printf("Result is an imaginary number");
+    }
+    else 
+    {
+        float temp;
+        float result;
+        result=number/2;
+        temp=0;
+        while(result!=temp)
+            {
+                temp=result;
+                result = ((number/temp)+temp)/2;
+                printf("The square root of %d is %f",number,result);
+       
+            }
+    }
 
 }
