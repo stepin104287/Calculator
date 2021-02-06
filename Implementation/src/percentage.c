875f8881fe78ed0fe0a488bf1e2b21a1816aab01
stepin104930 <69069498+stepin104930@users.noreply.github.com>
2021-02-06 16:31:21 +0530

    Update percentage.c

--- Implementation/src/percentage.c
+++ Implementation/src/percentage.c
@@ -2,9 +2,21 @@
 #include<stdio.h>
 
 float percentage(float num1, float num2)
+  
 {
    float total;
-   total=((num1*100)/num2);
-   printf("%f\n",total);
-   return total;
+   printf("Enter two numbers:\n");
+   if(num2==0)
+   {
+       printf("Undefined");
+   }
+   
+   else
+   {
+   //total=((num2-num1)*100)/num1;
+      total=((num1*num2)/100);
+      printf("%f",total);
+   //return total;
+   }
 }
+
