b8297b49db5b5ae6e7d4c0a1079efa47eb697810
stepin104974 <69135861+stepin-104974-sumit@users.noreply.github.com>
2021-02-06 15:00:45 +0530

    Create main_header.h

---
+++ Implementation/header/main_header.h
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdli>
+#include <math.h>
+
+
+#include "subtraction.h"
+#include "add.h"
+#include "cuberoot.h"
+#include "divide.h"
+#include "exponential.h"
+#include "modular_division.h"
+#include "multiplication.h"
+#include "percentage.h"
+#include "squareroot.h"
